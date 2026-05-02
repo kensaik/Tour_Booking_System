eb6f5a03d58a35d7729ae1184f0338fe6a70e83b
ManhLe <leduymanh634@gmail.com>
2026-05-02 17:44:19 +0700

    feat: implement admin blueprint for destination and company management and initialize app factory

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -31,8 +31,10 @@ def create_app(config_class=Config):
     # Register Blueprints here
     from src.routes import main_bp
     from src.routes.auth import auth_bp
+    from src.routes.admin import admin_bp
 
     app.register_blueprint(main_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(admin_bp)
 
     return app
